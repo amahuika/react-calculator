5d6b1d666b8b25c9dfa16b70d46039c9b7bea4dd
amahuika <amahuika@hotmail.com>
2022-10-25 10:17:57 +1300

    basic layout and logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [answer, setAnswer] = useState("");
+  const [first, setFirst] = useState("");
+  const [second, setSecond] = useState("");
+
+  function Calculate(i) {
+    const parseFirst = parseFloat(first);
+    const parseSecond = parseFloat(second);
+
+    switch (i) {
+      case "+":
+        setAnswer(parseFirst + parseSecond);
+        break;
+      case "-":
+        setAnswer(parseFirst - parseSecond);
+        break;
+      case "/":
+        setAnswer(parseFirst / parseSecond);
+        break;
+      case "X":
+        setAnswer(parseFirst * parseSecond);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  function Clear() {
+    setAnswer("");
+    setFirst("");
+    setSecond("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>Simple Calculator</h1>
+      <input
+        type="number"
+        value={first}
+        onChange={(event) => setFirst(event.target.value)}
+      />
+      <input
+        type="number"
+        value={second}
+        onChange={(event) => setSecond(event.target.value)}
+      />
+      =
+      <input type="number" defaultValue={answer} />
+      <div>
+        <button className="AppButton" onClick={() => Calculate("+")}>
+          +
+        </button>
+        <button className="AppButton" onClick={() => Calculate("-")}>
+          -
+        </button>
+        <button className="AppButton" onClick={() => Calculate("X")}>
+          X
+        </button>
+        <button className="AppButton" onClick={() => Calculate("/")}>
+          /
+        </button>
+        <button className="AppButton" onClick={Clear}>
+          Clear
+        </button>
+      </div>
     </div>
   );
 }
